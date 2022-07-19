cf2adfbb1ff46801bb8dba6eea1debc2fa617c03
gilgardosh <gilgardosh@gmail.com>
2022-07-19 10:19:56 +0300

    basic setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@theguild'],
+  plugins: ['eslint-plugin-simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+const { plugins, ...prettierConfig } = require('@theguild/prettier-config');
+
+module.exports = {
+  ...prettierConfig,
+  plugins: [...plugins, 'prettier-plugin-sql'],
+  // prettier-plugin-sql options
+  language: 'postgresql',
+  uppercase: true,
+};
