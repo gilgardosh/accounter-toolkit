e3220dcf295e4f5ea25ccc06f82eec61a58afd7d
Gil Gardosh <gilgardosh@gmail.com>
2022-07-21 20:49:13 +0300

    Add green invoice graphql (#34)
    * Add green-invoice-graphql
    
    * prettier
    
    * lint fixes, dependency updates
    
    * some minor non-related fixes

---
+++ packages/green-invoice-graphql/index.ts
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch';
+
+import { getMeshSDK, Sdk } from './.mesh';
+
+export const init = async (id: string, secret: string): Promise<{ sdk: Sdk }> => {
+  const authParams = {
+    id,
+    secret,
+  };
+  const greenInvoiceToken = await fetch('https://api.greeninvoice.co.il/api/v1/account/token', {
+    method: 'POST',
+    body: JSON.stringify(authParams),
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then(res => res.json())
+    .then((res: { token: string }) => res.token);
+
+  process.env.GREEN_INVOICE_AUTH_TOKEN = greenInvoiceToken;
+
+  const sdk = await getMeshSDK();
+
+  return { sdk };
+};
+
+export * from './.mesh';
