65f1290b8fe2c350f11dfb3d7b50523688c409de
gilgardosh <gilgardosh@gmail.com>
2023-03-26 14:34:18 +0300

    Enhance digit-only variables validation

--- packages/pcn874-generator/src/utils/data-handlers.ts
+++ packages/pcn874-generator/src/utils/data-handlers.ts
@@ -5,38 +5,54 @@ const digitsAdjuster = (value: string, length: number) => {
   return `${'0'.repeat(length)}${value}`.slice(-length);
 };
 
+const cleanNonDigitChars = (value: string): string => {
+  return value.replace(/\D/g, '');
+};
+
 export const transactionHandler = (transaction: Transaction, options: Options): Transaction => {
-  if (transaction.refNumber && transaction.refNumber.length !== 9) {
-    if (options.strict) {
+  if (transaction.refNumber) {
+    const sanitizedNumber = cleanNonDigitChars(transaction.refNumber);
+    if (
+      options.strict &&
+      (sanitizedNumber.length !== 9 || sanitizedNumber.length !== transaction.refNumber.length)
+    ) {
       throw new Error(
         `Expected Transaction refNumber to be of 9 digits, received "${transaction.refNumber}". ${
           transaction.refNumber.length > 9 ? `Using the last 9 digits.` : `Adding leading zeros.`
         }`,
       );
     }
-    transaction.refNumber = digitsAdjuster(transaction.refNumber, 9);
+    transaction.refNumber = digitsAdjuster(sanitizedNumber, 9);
   }
 
-  if (transaction.vatId && transaction.vatId.length !== 9) {
-    if (options.strict) {
+  if (transaction.vatId) {
+    const sanitizedNumber = cleanNonDigitChars(transaction.vatId);
+    if (
+      options.strict &&
+      (sanitizedNumber.length !== 9 || sanitizedNumber.length !== transaction.vatId.length)
+    ) {
       throw new Error(
         `Expected Transaction vatId to be of 9 digits, received "${transaction.vatId}". ${
-          transaction.vatId.length > 9 ? `Using the last 9 digits.` : `Adding leading zeros.`
+          sanitizedNumber.length > 9 ? `Using the last 9 digits.` : `Adding leading zeros.`
         }`,
       );
     }
-    transaction.vatId = digitsAdjuster(transaction.vatId, 9);
+    transaction.vatId = digitsAdjuster(sanitizedNumber, 9);
   }
 
-  if (transaction.refGroup && transaction.refGroup.length !== 4) {
-    if (options.strict) {
+  if (transaction.refGroup) {
+    const sanitizedNumber = cleanNonDigitChars(transaction.refGroup);
+    if (
+      options.strict &&
+      (sanitizedNumber.length !== 4 || sanitizedNumber.length !== transaction.refGroup.length)
+    ) {
       throw new Error(
         `Expected Transaction refGroup to be of 4 digits, received "${transaction.refGroup}". ${
-          transaction.refGroup.length > 9 ? `Using the last 4 digits.` : `Adding leading zeros.`
+          sanitizedNumber.length > 4 ? `Using the last 4 digits.` : `Adding leading zeros.`
         }`,
       );
     }
-    transaction.refGroup = digitsAdjuster(transaction.refGroup, 4);
+    transaction.refGroup = digitsAdjuster(sanitizedNumber, 4);
   }
 
   return transactionValidator(transaction, options);
@@ -44,16 +60,18 @@ export const transactionHandler = (transaction: Transaction, options: Options): 
 
 export const headerHandler = (header: Header, options: Options): Header => {
   if (header.licensedDealerId && header.licensedDealerId.length !== 9) {
-    if (options.strict) {
+    const sanitizedNumber = cleanNonDigitChars(header.licensedDealerId);
+    if (
+      options.strict &&
+      (sanitizedNumber.length !== 9 || sanitizedNumber.length !== header.licensedDealerId.length)
+    ) {
       throw new Error(
         `Expected Header licensedDealerId to be of 9 digits, received "${
           header.licensedDealerId
-        }". ${
-          header.licensedDealerId.length > 9 ? `Using the last 9 digits.` : `Adding leading zeros.`
-        }`,
+        }". ${sanitizedNumber.length > 9 ? `Using the last 9 digits.` : `Adding leading zeros.`}`,
       );
     }
-    header.licensedDealerId = digitsAdjuster(header.licensedDealerId, 9);
+    header.licensedDealerId = digitsAdjuster(sanitizedNumber, 9);
   }
 
   return headerValidator(header);
