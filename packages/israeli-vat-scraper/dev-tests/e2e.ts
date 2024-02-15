62e242c61c92038c26405f8922e06d26a2526d5b
gilgardosh <gilgardosh@gmail.com>
2024-02-15 16:17:15 +0200

    minor text fix

--- packages/israeli-vat-scraper/dev-tests/e2e.ts
+++ packages/israeli-vat-scraper/dev-tests/e2e.ts
@@ -1,7 +1,7 @@
 import { config } from 'dotenv';
 import { vatScraper } from '../src/index.js';
 
-config();
+config({ path: '../../.env' });
 
 const testRun = async () => {
   vatScraper(undefined, { years: [[2020, [5]]], visibleBrowser: true });
